5ee78c68aa4724374f71168cf26d0f8544314d10
Ramin <Ramin.rezaei77@gmail.com>
2022-04-23 07:46:22 +0430

    feat: config tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from 'reportWebVitals';
+
+import 'styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
