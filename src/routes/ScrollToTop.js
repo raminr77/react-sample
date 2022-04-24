5d5e39804117a2c0a0df0dd0915b2f93a22424ba
Ramin <Ramin.rezaei77@gmail.com>
2022-04-24 16:27:43 +0430

    fix: scroll to top when route changing

--- src/routes/ScrollToTop.js
+++ src/routes/ScrollToTop.js
@@ -1,17 +1,12 @@
 import { useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-const ScrollToTop = ({ history }) => {
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
-    });
-    return () => {
-      unlisten();
-    };
-  }, []);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return null;
-};
-
-export default withRouter(ScrollToTop);
+}
