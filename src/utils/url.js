1d3d9951a9c5c8550b078cd7dbaf2bfe0179883f
Ramin <Ramin.rezaei77@gmail.com>
2022-04-29 16:06:09 +0430

    feat: add backUrl

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,27 @@
+import QS from 'qs';
 import { useNavigate } from 'react-router-dom';
 
-export const redirect = ({ url }) => {
+export const getCurrentUrl = () => {
+  try {
+    return decodeURI(`${window.location.pathname}${window.location.search}`);
+  } catch (e) {
+    return '/';
+  }
+};
+
+export const attachObjectQueriesToUrl = (url = '/', object = {}, encode = false) => {
+  const currentQSIndex = url.indexOf('?');
+  if (currentQSIndex > -1) {
+    return `${url}&${QS.stringify(object, { encode })}`;
+  }
+  return `${url}?${QS.stringify(object, { encode })}`;
+};
+
+export const redirect = ({ url, backUrl }) => {
   const navigate = useNavigate();
+  const finalUrl = !!backUrl
+    ? attachObjectQueriesToUrl(url, { backUrl: backUrl || getCurrentUrl() })
+    : url;
+  if (!finalUrl) return;
   return navigate(url);
 };
