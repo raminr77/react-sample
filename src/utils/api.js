808baaa27abd847029f2008b8e1c49848c1b0ae2
Ramin <Ramin.rezaei77@gmail.com>
2022-05-14 02:45:22 +0430

    feat: Cancel Duplicate Request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,26 @@
 import { REQUEST_TYPE } from 'constants/RequestType';
 import { get, post, CancelToken } from 'services/api';
 
+const REQUEST_LIST = {};
+const MIN_TIME_FOR_DUPLICATE_REQUEST = 1000;
+
 export function apiRequestObject({ url, transformer, inputTransformer, type = REQUEST_TYPE.GET }) {
   const source = CancelToken.source();
   const apiCall = (data, options = {}) => {
+    // Cancel Duplicate Request
+    const REQUEST_HASH = `[${type}][${url}][${JSON.stringify(data)}]`;
+    if (!!REQUEST_LIST[REQUEST_HASH]) {
+      const diffTime = new Date().getTime() - REQUEST_LIST[REQUEST_HASH];
+      if (diffTime < MIN_TIME_FOR_DUPLICATE_REQUEST) {
+        delete REQUEST_LIST[REQUEST_HASH];
+        throw 'Request was cancelled';
+      } else {
+        REQUEST_LIST[REQUEST_HASH] = new Date().getTime();
+      }
+    } else {
+      REQUEST_LIST[REQUEST_HASH] = new Date().getTime();
+    }
+    // start New Request
     const moderatedOptions = { ...options };
     const transformedData = inputTransformer ? inputTransformer(data) : data;
     const dataIsFormData = transformedData instanceof FormData;
