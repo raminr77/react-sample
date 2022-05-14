1bfd993625989eb7fbe7ea069366fbc96cb72fc6
Ramin <Ramin.rezaei77@gmail.com>
2022-05-14 17:11:24 +0430

    feat: add vazir font

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,10 +4,7 @@ import { INDEX_PAGE_ROUTE } from 'routes/RedirectRoutes';
 
 export const NotFoundPage = () => {
   return (
-    <div
-      dir='ltr'
-      className='en-num-font flex-col h-screen w-screen flex items-center justify-center'
-    >
+    <div dir='ltr' className='flex-col h-screen w-screen flex items-center justify-center'>
       <h3 className='text-xl'>404 | Not Found</h3>
       <br />
       <Link to={INDEX_PAGE_ROUTE}>Back To Home</Link>
