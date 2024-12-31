66646c204dd05038b1e67b4bdef3b67c59b21613
Aman Verma <141578852+amanverma678@users.noreply.github.com>
2024-12-31 16:41:17 +0530

    Update renovate-config.js

--- .github/workflows/example/renovate-config.js
+++ .github/workflows/example/renovate-config.js
@@ -1,25 +1,24 @@
-module.exports = {
-  branchPrefix: 'main/',
-  username: 'amanverma678',
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
-  platform: 'github',
-  forkProcessing: 'enabled',
-  dryRun: 'full',
-  repositories: ["terraform-aws-vpc"],
-  packageRules: [
+{
+  "dryRun": "full",
+  "username": "renovate-release",
+  "gitAuthor": "Renovate Bot <bot@renovateapp.com>",
+  "onboarding": false,
+  "platform": "github",
+  "forkProcessing": "enabled",
+  "repositories": ['react-sample'],
+  "packageRules": [
     {
-      description: 'lockFileMaintenance',
-      matchUpdateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
+      "description": "lockFileMaintenance",
+      "matchUpdateTypes": [
+        "pin",
+        "digest",
+        "patch",
+        "minor",
+        "major",
+        "lockFileMaintenance"
       ],
-      dependencyDashboardApproval: false,
-      minimumReleaseAge: '0 days',
-    },
-  ],
-};
+      "dependencyDashboardApproval": false,
+      "minimumReleaseAge": 0
+    }
+  ]
+}
